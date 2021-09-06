d4113e2e04feaa4731b2648777cf57012abb5895
zhengchl <chuanlei.zheng@gmail.com>
2021-09-06 22:55:15 +0800

    DecoratorDemo init

---
+++ DecoratorDemo.cs
@@ -0,0 +1,47 @@
+namespace dp_dotnet {
+    public interface ITextNode {
+        void setText(string str);
+        string getText();
+    }
+
+    public class SpanNode : ITextNode {
+        private string text;
+        public SpanNode(string str) {
+            this.text = str;
+        }
+        public void setText(string str) {
+            this.text = str;
+        }
+        public string getText() {
+            return "<span>" + this.text + "</span>";
+        }
+    }
+
+    public abstract class NodeDecorator : ITextNode {
+        protected ITextNode node;
+        public NodeDecorator(ITextNode node) {
+            this.node = node;
+        }
+        public void setText(string str) {
+            this.node.setText(str);
+        }
+        public abstract string getText();
+    }
+
+    public class BoldDecorator : NodeDecorator {
+        public BoldDecorator(ITextNode node) : base(node) {
+        }
+        public override string getText() {
+            return "<b>" + this.node.getText() + "</b>";
+        }
+    }
+
+    public class UnderLineDecorator : NodeDecorator {
+        public UnderLineDecorator(ITextNode node) : base(node) {
+
+        }
+        public override string getText() {
+            return "<u>" + this.node.getText() + "</b>";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,22 @@ namespace dp_dotnet {
             InterfaceB adaptor = new AdaptorB(new ImplA());
             Console.WriteLine(adaptor.MethodB());
         }
+        static void DecoratorDemo() {
+            SpanNode span = new SpanNode("hellow world!");
+            ITextNode span1 = new BoldDecorator(new UnderLineDecorator(span));
+            ITextNode span2 = new UnderLineDecorator(new BoldDecorator(span));
+            Console.WriteLine(span1.getText());
+            Console.WriteLine(span2.getText());
+            span1.setText("change");
+            Console.WriteLine(span1.getText());
+            Console.WriteLine(span2.getText());
+
+        }
         static void Main(string[] args) {
             Console.WriteLine("Call AdaptorDemo");
             AdaptorDemo();
+            Console.WriteLine("Call DecoratorDemo");
+            DecoratorDemo();
         }
     }
 }
