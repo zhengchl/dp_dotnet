5e198e2b8b9ef38cdf7fbde2b56c2b882aec52ad
zhengchuanlei <zhengchuanlei@bytedance.com>
2021-09-07 15:41:53 +0800

    resp chain

--- Program.cs
+++ Program.cs
@@ -6,9 +6,20 @@ namespace dp_dotnet {
             InterfaceB adaptor = new AdaptorB(new ImplA());
             Console.WriteLine(adaptor.MethodB());
         }
+        static void ResponsibilityChainDemo() {
+            Request req1 = new Request("Walter", 1m);
+            Request req2 = new Request("Bob", 1m);
+            ManagerHandler managerHandler = new ManagerHandler();
+            HandlerChain chain = new HandlerChain();
+            chain.Add(managerHandler);
+            chain.Process(req1);
+            chain.Process(req2);
+        }
         static void Main(string[] args) {
             Console.WriteLine("Call AdaptorDemo");
             AdaptorDemo();
+            Console.WriteLine("Call ResponsibilityChainDemo");
+            ResponsibilityChainDemo();
         }
     }
 }
